eac9c2c871bcd896a70bfd581f69239ce53ddd55
Rohith Sv <svrohith9@gmail.com>
2023-07-26 15:38:28 -0500

    INSERT and FETCH IMAGES API calls from DB

---
+++ src/main/java/com/example/crudproject/controllers/ImageController.java
@@ -0,0 +1,60 @@
+package com.example.crudproject.controllers;
+
+import com.example.crudproject.dto.ImageEntity;
+import com.example.crudproject.repository.ImageRepository;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Optional;
+
+@RequestMapping("/api/image")
+@RestController
+public class ImageController {
+
+    private final ImageRepository imageRepository;
+
+    public ImageController(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<ImageEntity> uploadImage(@RequestParam("file") MultipartFile file, @RequestParam("name") String name) {
+        try {
+            String contentType = file.getContentType();
+            if(contentType == null || !contentType.startsWith("image/")){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+
+            byte[] imageData = file.getBytes();
+            ImageEntity imageEntity = new ImageEntity();
+            imageEntity.setImage(imageData);
+            imageEntity.setName(name);
+            imageEntity.setUploadDate(new Date());
+            imageEntity.setContentType(contentType);
+            imageRepository.save(imageEntity);
+            return ResponseEntity.ok().body(imageEntity);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Resource> getImage(@PathVariable Long id) {
+        Optional<ImageEntity> imageOptional = imageRepository.findById(id);
+        if (imageOptional.isPresent()) {
+            ImageEntity imageEntity = imageOptional.get();
+            ByteArrayResource resource = new ByteArrayResource(imageEntity.getImage());
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(imageEntity.getName()).build());
+            headers.setContentType(MediaType.parseMediaType(imageEntity.getContentType()));
+            return ResponseEntity.ok().headers(headers).body(resource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/example/crudproject/dto/ImageEntity.java
@@ -0,0 +1,30 @@
+package com.example.crudproject.dto;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "images")
+public class ImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Lob
+    @Column(name = "image", columnDefinition="BLOB")
+    private byte[] image;
+
+    @Column(name = "upload_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date uploadDate;
+
+    @Column(name = "content_type")
+    private String contentType;
+}
